8d737e6591e16be0fea3535bc6e7af7fdc0cca1e
taehee.rho <taeheerho@gmail.com>
2023-07-15 19:06:55 +0900

    🎨 style: Button UI 공통 컴포넌트 구현

---
+++ src/components/Button.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Button = (props) => {
+  const { type } = props;
+  return (
+    <ButtonStyled type={type ? type : "button"} {...props}>
+      {props.children}
+    </ButtonStyled>
+  );
+};
+
+const ButtonStyled = styled.button`
+  background-color: ${(props) => props.bgColor || "var(--black-color)"};
+  color: ${(props) => props.textColor || "white"};
+  padding: ${(props) => props.padding || "16px 0 16px 0"};
+  font-family: ${(props) => props.font || "Pretendard-Medium"};
+  cursor: pointer;
+  margin: 10px;
+  border-radius: 6px;
+  font-size: 16px;
+  width: 100%;
+  text-align: center;
+  transition: all 0.3s;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+
+  ${(props) =>
+    props.disabled &&
+    css`
+      background-color: var(--gray400-color);
+      cursor: default;
+
+      &:hover {
+        background-color: var(--gray400-color);
+      }
+    `}
+`;
+
+export default Button;
